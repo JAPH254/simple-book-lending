55ae5128858dc61f996c36032bdaa696f76c4440
JAPH254 <omanyujapheth@gmail.com>
2025-02-18 17:09:48 +0300

    feat: Improve borrowing and returning book notifications for better user clarity

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
 
   def borrow
     book = Book.find(params[:id])
-  
+
     if book.borrowings.where(returned_at: nil).exists?
       redirect_to book_path(book), alert: "This book is already borrowed."
     else
@@ -19,16 +19,15 @@ class BooksController < ApplicationController
       redirect_to profile_path, notice: "You have successfully borrowed #{book.title}. Due date: #{borrowing.due_date.strftime('%B %d, %Y')}."
     end
   end
-  
 
   def return_book
     borrowing = current_user.borrowings.find_by(book_id: params[:id], returned_at: nil)
 
     if borrowing
       borrowing.update(returned_at: Time.current)
-      redirect_to profile_path, notice: "Book returned successfully!"
+      redirect_to profile_path, notice: "You have successfully returned #{borrowing.book.title}!"
     else
-      redirect_to profile_path, alert: "You haven't borrowed this book."
+      redirect_to profile_path, alert: "You haven't borrowed this book or it was already returned."
     end
   end
 end
