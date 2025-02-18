3a4be211e786af8fd54a1d0fb4b58dbe5f151331
JAPH254 <omanyujapheth@gmail.com>
2025-02-18 15:34:07 +0300

    feat:Update Borrowing model to enforce unique book borrowing per user

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -2,14 +2,5 @@ class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :due_date, presence: true
-  validate  :book_availability
-
-  private
-
-  def book_availability
-    if book.borrowings.where(returned_at: nil).exists?
-      errors.add(:book, "is already borrowed")
-    end
-  end
+  validates :book_id, uniqueness: { scope: :returned_at, message: "can't be borrowed multiple times" }
 end
