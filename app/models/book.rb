565f4ba21509eea9642f508cdc7da971c9b9f518
JAPH254 <omanyujapheth@gmail.com>
2025-02-18 17:10:03 +0300

    feat: Add availability scope and default availability setting to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,22 @@
 class Book < ApplicationRecord
     validates :title, :author, :isbn, presence: true
     validates :isbn, uniqueness: true
-
+  
     has_many :borrowings
     has_many :users, through: :borrowings
-end
+  
+    scope :available, -> { where(available: true) }
+  
+    after_initialize :set_default_availability, if: :new_record?
+  
+    def available?
+      available
+    end
+  
+    private
+  
+    def set_default_availability
+      self.available = true if available.nil?
+    end
+  end
+  
